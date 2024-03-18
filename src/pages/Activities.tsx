350a4bb70d806e19e649682a5c0a38a7476503c7
Nouhayla-code <nouhayla@hotmail.dk>
2024-03-18 16:48:59 +0100

    :(

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -1,14 +1,16 @@
 import NavHeader from "../components/NavHeader";
-import Activity from "../components/Activity";
+import ActivityItem from "../components/ActivityItem";
 import { useEffect, useState } from "react";
+import { Activity } from "../service/apiFacade";
 import { getActivities } from "../service/apiFacade";
 
 export default function Activities() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     getActivities().then((data) => {
       console.log(data);
+      setActivities(data);
     });
   }, []);
 
@@ -19,6 +21,10 @@ export default function Activities() {
         <h2 className="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl">
           Activities
         </h2>
+
+        {activities.map((activity) => (
+          <ActivityItem key={activity.name} activity={activity} />
+        ))}
       </div>
     </>
   );
